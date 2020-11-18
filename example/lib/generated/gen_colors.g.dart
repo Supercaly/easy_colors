01f8c9bd0b05be34dd6f05bbfecfbcc92349df77
supercaly <djonline96@gmail.com>
2020-11-18 12:34:01 +0100

    Add generated file gen_colors.g.dart in example

---
+++ example/lib/generated/gen_colors.g.dart
@@ -0,0 +1,13 @@
+// DO NOT EDIT. This is code generated via package:easy_colors/generate.dart
+
+import 'package:flutter/painting.dart';
+
+class GeneratedEasyColors {
+  const GeneratedEasyColors._();
+
+  static const Color black = Color(0xFF000000);
+
+  static const Color white = Color(4294967295);
+
+  static const Color red = Color(0xFFCD5C5C);
+}
